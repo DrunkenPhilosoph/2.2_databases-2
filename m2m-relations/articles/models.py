3025354a30e267c60c9815ce7a415b105504ee9f
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2024-04-01 00:39:38 +0300

    Update models.py

--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -7,10 +7,23 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-
+    tags = models.ManyToManyField('Tag', through='Scope')
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        # ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=50, verbose_name='Название')
+
+class Scope(models.Model):
+    main = models.BooleanField(default=False, verbose_name='Основной')
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Tag_id')
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Article_id')
+
+
+
+
