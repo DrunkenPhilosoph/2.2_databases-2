1c2d687bd67211e0197212c19160012f4511a4fa
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2024-04-01 19:02:48 +0300

    Update models.py

--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -7,7 +7,8 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-    tags = models.ManyToManyField('Tag', through='Scope')
+    tags = models.ManyToManyField('Tag', through='scope')
+
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
@@ -16,14 +17,29 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+
 class Tag(models.Model):
-    tag_name = models.CharField(max_length=50, verbose_name='Название')
+    name = models.CharField(max_length=50, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def is_main(self):
+        scope = self.scopes.first()
+        return scope.main
+
 
 class Scope(models.Model):
     main = models.BooleanField(default=False, verbose_name='Основной')
-    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Tag_id')
-    article_id = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Article_id')
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Раздел', related_name='scopes')
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Статья', related_name='scopes')
 
-
-
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
 
