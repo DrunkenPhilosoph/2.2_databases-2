fe45e5a953ebdfdf4fbf039999633c46164e4603
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2024-04-01 19:04:00 +0300

    Update admin.py

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
 
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        form_lst = []
+        for form in self.forms:
+            dict_form = form.cleaned_data
+            print(type(dict_form), dict_form)
+            if dict_form['main'] == True:
+                form_lst.append(dict_form['main'])
+        if len(form_lst) >= 2:
+            raise ValidationError('Основной раздел может быть только 1')
+        else:
+            return super().clean()
+
+class ScopeRelationInline(admin.TabularInline):  # or admin.StackedInline
+    model = Scope
+    formset = RelationshipInlineFormset
+    extra = 0
+
+class TagAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeRelationInline]
+
+admin.site.register(Tag)
+
